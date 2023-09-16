df9a1e92838d085957219aa05f8e00a92038974f
Victor Powilleit <victor.powilleit@gmail.com>
2023-09-16 02:11:05 -0300

    Finished

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import {fastify} from "fastify";
 import {getAllPromptsRoute} from "./routes/get-all-prompts";
 import {uploadVideoRoute} from "./routes/upload-video";
+import {createTranscriptionRoute} from "./routes/create-transcription";
+import {generateAiCompletionRoute} from "./routes/generate-ai-completion";
+import * as fastifyCors from "@fastify/cors";
 
 const app = fastify()
 
+app.register(fastifyCors, {
+    origin:'*'
+})
+
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
 
 app.listen({
     port: 3333
